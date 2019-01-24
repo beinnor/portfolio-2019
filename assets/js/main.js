b74e9271cd4a9219bb993ecd5cc1ffee0c422384
Ronnie <16521174+beinnor@users.noreply.github.com>
2019-01-24 22:20:49 +0100

    fixed alot of things, forgot to commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,24 @@
-// Formspree contact form.
-let contactform = document.getElementById("contactform");
-contactform.setAttribute(
-  "action",
-  "//formspree.io/" +
-    "ronnie.bjorkelund" +
-    "+portfolio" +
-    "@" +
-    "gmail" +
-    "." +
-    "com"
-);
+const navButton = document.querySelector(".nav-button");
+const overlay = document.querySelector(".overlay");
+
+navButton.addEventListener("click", toggleMenuOverlay);
+
+let showOverlay = false;
+
+function toggleMenuOverlay() {
+  if (!showOverlay) {
+    overlay.classList.add("show");
+    navButton.firstChild.classList.replace("fa-bars", "fa-times");
+    navButton.classList.add("close");
+
+    // Set menu state
+    showOverlay = true;
+  } else {
+    overlay.classList.remove("show");
+    navButton.firstChild.classList.replace("fa-times", "fa-bars");
+    navButton.classList.remove("close");
+
+    // Set menu state
+    showOverlay = false;
+  }
+}
